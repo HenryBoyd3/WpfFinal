93a5b0967d42c58bd52632637e164f031e736e0b
Dorkwoodgames <dorkwoodgames@gmail.com>
2021-03-13 13:22:55 -0800

    Add project files.

---
+++ secondwindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFinal
+{
+    /// <summary>
+    /// Interaction logic for secondwindow.xaml
+    /// </summary>
+    public partial class secondwindow : Window
+    {
+        public secondwindow()
+        {
+            InitializeComponent();
+        }
+        private void firstWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new MainWindow(); 
+            newForm.Show(); 
+            this.Close(); 
+        }
+        private void thirdWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new thirdwindow();
+            newForm.Show();
+            this.Close(); 
+        }
+    }
+}
---
+++ thirdwindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfFinal
+{
+    /// <summary>
+    /// Interaction logic for thirdwindow.xaml
+    /// </summary>
+    public partial class thirdwindow : Window
+    {
+        public thirdwindow()
+        {
+            InitializeComponent();
+        }
+
+        private void secondWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new secondwindow();
+            newForm.Show();
+            this.Close();
+        }
+        private void firstWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var newForm = new MainWindow(); 
+            newForm.Show();
+            this.Close();
+        }
+    }
+}
